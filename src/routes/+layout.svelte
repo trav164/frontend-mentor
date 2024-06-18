7ff089f0fec645afb684485404caebc239c47e43
trav164 <travis164@gmail.com>
2024-06-18 22:14:46 +0100

    WIP

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="container mx-auto flex h-screen items-center justify-center">
+	<slot></slot>
+</div>
+
+<style></style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col gap-4 bg-slate-200 p-6">
+	<span>Links</span>
+	<div>
+		<ul class="">
+			<li><a class="font-medium underline" href="/contact">Contact</a></li>
+		</ul>
+	</div>
+</div>
